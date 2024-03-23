94c5489068df490d53f9229b1eb66e62de9060a5
veerendranadh koppula <96357491+veerendranadhkoppula@users.noreply.github.com>
2024-03-23 17:57:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var typed = new Typed(".text", {
+      strings: ["Python Fullstack Developer", "Web designer"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 1000,
+      loop: true,
+    });
+  });
